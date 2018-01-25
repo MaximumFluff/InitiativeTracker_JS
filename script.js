d8adf88baa56dd9c816bfb006992c0f0f4bd1fd2
Alex Jacobs <a1603181@myy.haaga-helia.fi>
2018-01-25 19:13:48 +0200

    Fixes

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+class Player
+{
+    constructor(name, ac, hp, init)
+    {
+        this.name = name;
+        this.ac = ac;
+        this.hp = hp;
+        this.initiative = init;
+    }
+}
+
 addCreature = () =>
 {
+    console.log("I am doing a thing!");
     let name = document.getElementById("name").value;
     let ac = document.getElementById("ac").value;
     let hp = document.getElementById("hp").value;
@@ -11,6 +23,8 @@ addCreature = () =>
 
 printCreature = (creature) =>
 {
-    const printString = "<tr><td>" + creature.name + "</td>" + "<td>" + creature.ac + "</td>" + "<td>" + creature.hp + "</td><td>" + creature.initiative + "</td></tr>";
+    let printString = "";
+    printString += "<tr><td>" + creature.name + "</td>" + "<td>" + creature.ac + "</td>" + "<td>" + creature.hp + "</td>" + "<td>" + creature.initiative + "</td></tr>";
     document.getElementById("list").innerHTML += printString;
+    console.log(printString);
 }
